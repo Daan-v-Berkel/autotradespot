56c1b1165363cbc928b02b64c8e3917fbd52823e
Daan van Berkel <daan.van.berkel@keronic.com>
2024-01-19 09:00:33 +0100

    light cleanup

--- portfolios/lease_finder_app/caradata_script.py
+++
@@ -1,6 +0,0 @@
-import pandas
-
-df = pandas.read_csv(
-    "/Users/daan.van.berkel/Documents/Projects/Django-docker/lease_finder/lease_finder_app/temp_files/datasets/make-model.csv",
-    sep=";",
-)
