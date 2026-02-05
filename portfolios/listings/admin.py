1b43591b05dd20b5133297a66b38eb297c280cd3
Daan-v-Berkel <daanvb1990@hotmail.com>
2026-02-05 16:42:56 -0500

    linitng fixes

--- portfolios/listings/admin.py
+++ portfolios/listings/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import *
+from .models import CarDetails, Listing, PricingModelBuy, PricingModelLease
 
 admin.site.register(Listing)
 admin.site.register(CarDetails)
